eb9d19133a6a321685bac35ae6d08e2cfe84b8a3
Andres Olaya <47861034+cadenaandres@users.noreply.github.com>
2020-02-07 15:09:24 -0500

    Update: Se Modifica SQL

--- 3- Desarrollo/01- Base Datos MySQL/06- Consultas_Y_Joins.sql
+++ 3- Desarrollo/01- Base Datos MySQL/06- Consultas_Y_Joins.sql
@@ -1,4 +1,4 @@
-use Kyukeisho;
+USE Kyukeisho;
 
 -- CONSULTAS Y JOINS 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -9,32 +9,32 @@ SELECT id_usuario, primer_nombre_usuario FROM Kyukeisho.usuario;
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Lista de usuarios registrados con el id_usuario diferente a AB y AC ------------------------
-select * from kyukeisho.usuario where id_usuario like 'AA%';
+SELECT * FROM kyukeisho.usuario WHERE id_usuario LIKE 'AA%';
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Cantidad de usuarios registrados -----
-select count(*) from kyukeisho.usuario;
+SELECT count(*) FROM kyukeisho.usuario;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- lista de administradores registrados -----
-select * from Kyukeisho.administrador;
+SELECT * FROM Kyukeisho.administrador;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Listado de ventas  Ordenado por fecha y hora----------------
 SELECT hora_entrada, hora_compra, nombre_producto, cantidad_producto, precio_unitario_producto, nombre_servicio, precio_servicio, precio_total
 FROM Kyukeisho.factura_productos 
-join Kyukeisho.factura on 
+JOIN Kyukeisho.factura ON 
 factura_productos.id_factura= factura.id_factura
-join kyukeisho.productos on
+JOIN kyukeisho.productos ON
 productos.id_producto=factura_productos.id_producto
-join kyukeisho.servicios_factura on
+JOIN kyukeisho.servicios_factura ON
 factura.id_factura=servicios_factura.id_factura
-join kyukeisho.servicios on
+JOIN kyukeisho.servicios ON
 servicios.id_servicio=servicios_factura.id_servicio
-order by hora_compra DESC;
+ORDER BY hora_compra DESC;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -43,91 +43,90 @@ order by hora_compra DESC;
 SELECT primer_nombre_usuario, segundo_nombre_usuario, primer_apellido_usuario, segundo_apellido_usuario, correo_usuario, hora_entrada, hora_compra, nombre_producto, 
 cantidad_producto, precio_unitario_producto, nombre_servicio, precio_servicio, precio_total
 FROM Kyukeisho.factura_productos 
-join Kyukeisho.factura on 
+JOIN Kyukeisho.factura ON 
 factura_productos.id_factura= factura.id_factura
-join kyukeisho.productos on
+JOIN kyukeisho.productos ON
 productos.id_producto=factura_productos.id_producto
-join kyukeisho.servicios_factura on
+JOIN kyukeisho.servicios_factura ON
 factura.id_factura=servicios_factura.id_factura
-join kyukeisho.servicios on
+JOIN kyukeisho.servicios ON
 servicios.id_servicio=servicios_factura.id_servicio
-join factura_compra on 
+JOIN factura_compra ON 
 factura.id_factura= factura_compra.id_factura
-join usuario on 
+JOIN usuario ON 
 factura_compra.id_usuario= usuario.id_usuario;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Lista de productos y categoría (inventario)
-
-select nombre_producto,cantidad_producto,precio_unitario_producto,nombre_tipo_producto from Kyukeisho.productos join Kyukeisho.tipo_producto on productos.id_tipo_producto = tipo_producto.id_tipo_producto;
+SELECT nombre_producto,cantidad_producto,precio_unitario_producto,nombre_tipo_producto FROM Kyukeisho.productos JOIN Kyukeisho.tipo_producto ON productos.id_tipo_producto = tipo_producto.id_tipo_producto;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Cantidad total de productos registrados
-select count(*) from Kyukeisho.productos;
+SELECT count(*) FROM Kyukeisho.productos;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Lista de categorías de productos
-select nombre_tipo_producto from Kyukeisho.tipo_producto;
+SELECT nombre_tipo_producto FROM Kyukeisho.tipo_producto;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --  Cantidad de categorías
-select count(*) from Kyukeisho.tipo_producto;
+SELECT count(*) FROM Kyukeisho.tipo_producto;
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --  Nombres y apellidos, id del cliente que realizó la compra de mayor valor
-select max(precio_total), primer_nombre_usuario, segundo_nombre_usuario, primer_apellido_usuario, segundo_apellido_usuario
-from Kyukeisho.usuario 
-join Kyukeisho.factura_compra 
-on usuario.id_usuario = factura_compra.id_usuario 
-join Kyukeisho.factura 
-on factura.id_factura = factura_compra.id_factura ;
+SELECT max(precio_total), primer_nombre_usuario, segundo_nombre_usuario, primer_apellido_usuario, segundo_apellido_usuario
+FROM Kyukeisho.usuario 
+JOIN Kyukeisho.factura_compra 
+ON usuario.id_usuario = factura_compra.id_usuario 
+JOIN Kyukeisho.factura 
+ON factura.id_factura = factura_compra.id_factura ;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Nombres y apellidos,  id del cliente que realizó la compra de menor valor -----------------
-select min(precio_total) 
-from Kyukeisho.usuario 
-left join Kyukeisho.factura_compra
-on usuario.id_usuario =factura_compra.id_usuario
-right join Kyukeisho.factura
-on factura_compra.id_factura = factura.id_factura;
+SELECT min(precio_total) 
+FROM Kyukeisho.usuario 
+LEFT JOIN Kyukeisho.factura_compra
+ON usuario.id_usuario =factura_compra.id_usuario
+RIGHT JOIN Kyukeisho.factura
+ON factura_compra.id_factura = factura.id_factura;
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Ordenar usarios, productos, categorías (ASC)-------------------------------
 
-	SELECT primer_nombre_usuario, segundo_nombre_usuario, primer_apellido_usuario, segundo_apellido_usuario, hora_entrada, hora_compra, nombre_producto, nombre_tipo_producto
-	FROM Kyukeisho.usuario
-	join Kyukeisho.factura_compra on
-	usuario.id_usuario=factura_compra.id_usuario
-	join Kyukeisho.factura on 
-	factura_compra.id_factura= factura.id_factura
-	join Kyukeisho.factura_productos on
-	factura.id_factura=factura_productos.id_factura
-	join kyukeisho.productos on
-	productos.id_producto=factura_productos.id_producto
-	join kyukeisho.tipo_producto on
-	tipo_producto.id_tipo_producto=productos.id_tipo_producto
-	order by hora_compra ASC;
+SELECT primer_nombre_usuario, segundo_nombre_usuario, primer_apellido_usuario, segundo_apellido_usuario, hora_entrada, hora_compra, nombre_producto, nombre_tipo_producto
+FROM Kyukeisho.usuario
+JOIN Kyukeisho.factura_compra ON
+usuario.id_usuario=factura_compra.id_usuario
+JOIN Kyukeisho.factura ON 
+factura_compra.id_factura= factura.id_factura
+JOIN Kyukeisho.factura_productos ON
+factura.id_factura=factura_productos.id_factura
+JOIN kyukeisho.productos ON
+productos.id_producto=factura_productos.id_producto
+JOIN kyukeisho.tipo_producto ON
+tipo_producto.id_tipo_producto=productos.id_tipo_producto
+ORDER BY hora_compra ASC;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Ordenar ventas por fecha (Desde la más actual a la más antigua)
 SELECT primer_nombre_usuario, segundo_nombre_usuario, primer_apellido_usuario, segundo_apellido_usuario, hora_entrada, hora_compra, nombre_producto, nombre_tipo_producto
 FROM Kyukeisho.usuario
-join Kyukeisho.factura_compra on
+JOIN Kyukeisho.factura_compra ON
 usuario.id_usuario=factura_compra.id_usuario
-join Kyukeisho.factura on 
+JOIN Kyukeisho.factura ON 
 factura_compra.id_factura= factura.id_factura
-join Kyukeisho.factura_productos on
+JOIN Kyukeisho.factura_productos ON
 factura.id_factura=factura_productos.id_factura
-join kyukeisho.productos on
+JOIN kyukeisho.productos ON
 productos.id_producto=factura_productos.id_producto
-join kyukeisho.tipo_producto on
+JOIN kyukeisho.tipo_producto ON
 tipo_producto.id_tipo_producto=productos.id_tipo_producto
-order by hora_compra DESC;
+ORDER BY hora_compra DESC;
